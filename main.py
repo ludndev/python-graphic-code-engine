f58a8b648b19e9aab03658231486ecfe58f22535
ludndev <ludndev@gmail.com>
2024-05-09 19:16:05 +0100

    feat: add main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Created: 10/10/2020
+Author: Judicaël AHYI <ludndev>
+Email: hello@judicael-ahyi.com
+License: MIT
+GitHub: https://github.com/ludndev/python-graphic-code-engine
+Description: implementing a custom graphic code generator inspire by https://github.com/AlexandrGraschenkov/QRCodes
+"""
+
+
+def main():
+    print("python-graphic-code-engine")
+
+
+if __name__ == "__main__":
+    main()
